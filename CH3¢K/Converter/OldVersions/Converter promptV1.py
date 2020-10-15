8019981de13799f43a24c852a97ca2297313b69c
Sean P. Myrick V19.0.4.2 <65933340+seanpm2001@users.noreply.github.com>
2020-10-15 13:03:50 -0700

    Converter promptV1.py
    An archive of the first version of the converter prompt script file.

---
+++ CH3¢K/Converter/OldVersions/Converter promptV1.py
@@ -0,0 +1,4 @@
+print ("Converter prompt")
+print ("Version 1.00 build 1")
+usrinput1 = str(input("Convert cryptocurrency to CH3CKS here. Type \ConType for cryptocurrency type, type \Concount to enter amount, then press Y"))
+
